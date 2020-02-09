0f319339fbb725f4e8e263f824e1a3b136666d29
Viachaslau Kuhtyuk <wenslauv@gmail.com>
2020-02-09 21:28:40 +0300

    initial test suite

---
+++ tests/sources/runner.cpp
@@ -0,0 +1,15 @@
+
+
+#include "../../include/Linq.hpp"
+
+#include "../../third-party/unittest-cpp/UnitTest++/UnitTest++.h"
+
+
+#include <iostream>
+
+
+
+int main(int, const char *[])
+{
+   return UnitTest::RunAllTests();
+}
